e73bbbb9f3a807a7cf3b4d8752ef3d023570d5ba
Aditya Gupta <adityaakadynamo@gmail.com>
2016-05-13 13:18:06 +0530

    HomeFragment : Showing the Theme Name which is currently being changes

--- app/src/main/java/projekt/dashboard/layers/fragments/HomeFragment.java
+++ app/src/main/java/projekt/dashboard/layers/fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package projekt.dashboard.layers.fragments;
 
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -16,6 +17,11 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import java.util.StringTokenizer;
 
 import butterknife.ButterKnife;
 import projekt.dashboard.layers.BuildConfig;
@@ -60,22 +66,87 @@ public class HomeFragment extends BasePageFragment {
         } else {
             status_message.setTextColor(getResources().getColor(R.color.attention_color));
             status_message.setText(getResources().getString(R.string.homepage_rom_not_supported));
+            MaterialDialog md = new MaterialDialog.Builder(getActivity())
+                    .title("Layers ROM missing")
+                    .content("you are missing one of the below prerequisites:-\n1. Layers Rom\n2. Layers Manager")
+                    .positiveText("Download Layers Manager")
+                    .negativeText("Cancel")
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(MaterialDialog dialog, DialogAction which) {
+                            Intent intent_rrolayers = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.lovejoy777.rroandlayersmanager"));
+                            startActivity(intent_rrolayers);
+                        }
+                    })
+                    .onNegative(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(MaterialDialog dialog, DialogAction which) {
+                        }
+                    })
+                    .dismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+                    })
+                    .show();
         }
 
         TextView theme_message = (TextView) inflation.findViewById(R.id.theme_message);
-        if (new LayersFunc(getActivity()).checkThemeMainSupported(getActivity()) && new LayersFunc(getActivity()).checkThemeSysSupported(getActivity())) {
-            theme_message.setTextColor(getResources().getColor(R.color.attention_color_green));
-            theme_message.setText(getResources().getString(R.string.homepage_theme_full_supported));
-            Snackbar snack = Snackbar.make(inflation, prefs.getString("dashboard_username",
-                    getResources().
-                            getString(R.string.
-                                    homepage_dashboard_app_development_status_default_username))
-                            + getResources().
-                            getString(R.string.homepage_dashboard_app_development_status)
-                            + " (" + BuildConfig.VERSION_NAME + ")",
-                    Snackbar.LENGTH_SHORT);
-            snack.show();
-        } else if (new LayersFunc(getActivity()).checkThemeMainSupported(getActivity())) {
+        if (LayersFunc.themesystemui != "Nill" && LayersFunc.themeframework != "Nill" && LayersFunc.downloaded) {
+            if (new LayersFunc(getActivity()).checkThemeMainSupported(getActivity())&&new LayersFunc(getActivity()).checkThemeSysSupported(getActivity())) {
+                theme_message.setTextColor(getResources().getColor(R.color.attention_color_green));
+                theme_message.setText(getResources().getString(R.string.homepage_theme_full_supported));
+                Snackbar snack = Snackbar.make(inflation, prefs.getString("dashboard_username",
+                        getResources().
+                                getString(R.string.
+                                        homepage_dashboard_app_development_status_default_username))
+                                + getResources().
+                                getString(R.string.homepage_dashboard_app_development_status)
+                                + " (" + BuildConfig.VERSION_NAME + ")",
+                        Snackbar.LENGTH_SHORT);
+                snack.show();
+            } else {
+                theme_message.setTextColor(getResources().getColor(R.color.attention_color_green));
+                String themename = LayersFunc.themeframework;
+                StringTokenizer stringTokenizer = new StringTokenizer(themename, "_");
+                theme_message.setText(stringTokenizer.nextToken() + " Detected !!");
+                Snackbar snack = Snackbar.make(inflation, prefs.getString("dashboard_username",
+                        getResources().
+                                getString(R.string.
+                                        homepage_dashboard_app_development_status_default_username))
+                                + getResources().
+                                getString(R.string.homepage_dashboard_app_development_status)
+                                + " (" + BuildConfig.VERSION_NAME + ")",
+                        Snackbar.LENGTH_SHORT);
+                snack.show();
+                MaterialDialog md = new MaterialDialog.Builder(getActivity())
+                        .title("Theme other than Akzent is found")
+                        .content("Changing colors over the fly ,without reboot.What do you think.But let me tell you something,its possible using akzent.")
+                        .positiveText("Download Akzent")
+                        .negativeText("Later")
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(MaterialDialog dialog, DialogAction which) {
+                                if (new LayersFunc(getActivity()).isAppInstalled(getActivity(), "com.chummy.aditya.materialdark.layers.donate")) {
+                                    startActivity(new Intent().setComponent(new ComponentName("com.lovejoy777.rroandlayersmanager", "com.lovejoy777.rroandlayersmanager.MainActivity")));
+                                } else {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.chummy.aditya.materialdark.layers.donate")));
+                                }
+                            }
+                        })
+                        .onNegative(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(MaterialDialog dialog, DialogAction which) {
+                            }
+                        })
+                        .dismissListener(new DialogInterface.OnDismissListener() {
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                            }
+                        })
+                        .show();
+            }
+        } else if (new LayersFunc(getActivity()).checkThemeMainSupported(getActivity()) && LayersFunc.downloaded) {
             theme_message.setTextColor(getResources().getColor(R.color.attention_color));
             theme_message.setText(getResources().getString(R.string.homepage_theme_half_supported));
             Snackbar snack = Snackbar.make(inflation, prefs.getString("dashboard_username",
@@ -103,7 +174,7 @@ public class HomeFragment extends BasePageFragment {
                     }).show();
                 }
             }, Snackbar.LENGTH_SHORT + 1000);
-        } else if (new LayersFunc(getActivity()).checkThemeSysSupported(getActivity())) {
+        } else if (new LayersFunc(getActivity()).checkThemeSysSupported(getActivity()) && LayersFunc.downloaded) {
             theme_message.setTextColor(getResources().getColor(R.color.attention_color));
             theme_message.setText(getResources().getString(R.string.homepage_theme_half_supported));
             Snackbar snack = Snackbar.make(inflation, prefs.getString("dashboard_username",
