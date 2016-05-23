3fc38bbfa64648d8271e92416c7a3209461b2a75
Aditya Gupta <adityaakadynamo@gmail.com>
2016-05-23 10:55:01 +0530

    Ummmmm :P

---
+++ app/src/main/java/projekt/dashboard/layers/util/CardStackPrefs.java
@@ -0,0 +1,39 @@
+package projekt.dashboard.layers.util;
+
+import android.content.Context;
+
+import com.mutualmobile.cardstack.CardStackLayout;
+import com.tramsun.libs.prefcompat.Pref;
+
+import projekt.dashboard.layers.R;
+
+public class CardStackPrefs {
+
+    public static final String SHOW_INIT_ANIMATION = "showInitAnimation";
+    public static final String PARALLAX_ENABLED = "parallaxEnabled";
+    public static final String PARALLAX_SCALE = "parallaxScale";
+    public static final String CARD_GAP = "cardGap";
+    public static final String CARD_GAP_BOTTOM = "cardGapBottom";
+
+    public static boolean isShowInitAnimationEnabled() {
+        return Pref.getBoolean(SHOW_INIT_ANIMATION, CardStackLayout.SHOW_INIT_ANIMATION_DEFAULT);
+    }
+
+    public static boolean isParallaxEnabled() {
+        return true;
+    }
+
+    public static int getParallaxScale() {
+        return -10;
+    }
+
+    public static int getCardGap(Context context) {
+        int cardGapDimenInDp = (int) (context.getResources().getDimension(R.dimen.card_gap) / context.getResources().getDisplayMetrics().density);
+        return Pref.getInt(CARD_GAP, cardGapDimenInDp);
+    }
+
+    public static int getCardGapBottom(Context context) {
+        int cardGapBottomDimenInDp = (int) (context.getResources().getDimension(R.dimen.card_gap_bottom) / context.getResources().getDisplayMetrics().density);
+        return Pref.getInt(CARD_GAP_BOTTOM, cardGapBottomDimenInDp);
+    }
+}
